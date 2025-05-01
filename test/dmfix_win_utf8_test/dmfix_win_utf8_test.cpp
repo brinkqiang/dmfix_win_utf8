c8315fcaad2a2466f50fc5c8412b3be870bc6fb4
brinkqiang <brink.qiang@gmail.com>
2025-05-01 22:17:54 +0800

    add

--- test/dmfix_win_utf8_test/dmfix_win_utf8_test.cpp
+++ test/dmfix_win_utf8_test/dmfix_win_utf8_test.cpp
@@ -1,7 +1,25 @@
 
 #include "dmfix_win_utf8.h"
+#include "gtest.h"
+#include <iostream>
 
-int main( int argc, char* argv[] ) {
+TEST(ConsoleEncodingTest, CodePageSettings) {
+#ifdef _WIN32
+    EXPECT_EQ(GetConsoleOutputCP(), 65001);
+    EXPECT_EQ(GetConsoleCP(), 65001);
+#else
+    const char* locale = std::setlocale(LC_ALL, nullptr);
+    EXPECT_NE(std::strstr(locale, "utf8"), nullptr);
+#endif
+}
 
-    return 0;
+TEST(ConsoleEncodingTest, SingletonPattern) {
+    EXPECT_TRUE(std::is_abstract_v<ConsoleEncoding>);
+    EXPECT_TRUE(std::is_copy_constructible_v<ConsoleEncoding> == false);
+    EXPECT_TRUE(std::is_move_constructible_v<ConsoleEncoding> == false);
+}
+
+TEST(ConsoleEncodingTest, cout) {
+
+    std::cout << "中文测试" << std::endl;
 }
